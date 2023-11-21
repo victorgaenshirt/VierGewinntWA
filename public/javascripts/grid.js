010e2c10e0657b6169214996526d3cc72afea769
Orkan23 <orkan2315@gmail.com>
2023-11-21 09:34:00 +0100

    playground with JS

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -1,22 +1,44 @@
 const grid = document.getElementById("grid");
 const gridsize = Number(document.getElementById("gridsize").innerHTML)
 
-for (let col = 0; col < gridsize; col++) {
-    const column = document.createElement("div")
-    column.className = "column"
+for (let iRow = 0; iRow < gridsize; iRow++) {
+    const oRow = document.createElement("div")
+    oRow.className = "row d-flex justify-content-evenly"
 
-    for (let r = gridsize - 1; r >= 0; r--) {
-        const row = document.createElement("div")
-        row.className = "circle"
+    for (let iCol = 0; iCol < gridsize; iCol++) {
+        const oCellDiv = document.createElement("div");
+        oCellDiv.className = "col-1";
+        const oCellSpan = document.createElement("span");
+        oCellSpan.className = "col bi bi-circle-fill";
+        oCellSpan.style = "font-size: 2.5em";
 
 
-        const color = document.getElementById("x" + col + "y" + r).innerHTML
-        row.classList.add(color)
-        row.addEventListener('click', function () {
-            playMove(col)
+        const sColor = document.getElementById(`${iRow}.${iCol}`).innerHTML
+        console.log((sColor))
+        if (sColor === "RED") {
+            oCellSpan.classList.add("text-warning")
+        } else if (sColor === "YELLOW") {
+            oCellSpan.classList.add("text-danger")
+        } else {
+            oCellSpan.classList.add("text")
+        }
+        oCellDiv.addEventListener('click', function () {
+            playMove(iCol)
         });
-        column.appendChild(row)
+        oCellDiv.appendChild(oCellSpan)
+        oRow.appendChild(oCellDiv)
 
     }
-    grid.appendChild(column)
+    grid.appendChild(oRow)
+}
+
+function playMove(column) {
+    fetch("/insert/" + column, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: "",
+    }).then(() => location.reload());
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,12 @@
-            function playMove(column) {
-                fetch("/insert/" + column, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: "",
-                }).then(() => location.reload());
-            }
 
-            function newGame(type) {
-                fetch("/newGame/" + type, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: "",
-                }).then(() => location.reload());
-            }
+
+function newGame(type) {
+    fetch("/newGame/" + type, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: "",
+    }).then(() => location.reload());
+}
