f16a4ec578854c8b763335baf2d02893ea077aa5
victorgaenshirt <vgaenshirt@posteo.de>
2023-11-26 01:53:58 +0100

    changed to Json Values

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,8 +3,12 @@ package controllers
 import com.google.inject.Guice
 import de.htwg.se.VierGewinnt.VierGewinntModule
 import de.htwg.se.VierGewinnt.controller.controllerComponent.ControllerInterface
+import de.htwg.se.VierGewinnt.model.fileIoComponent.FileIOInterface
+import de.htwg.se.VierGewinnt.model.playgroundComponent.PlaygroundInterface
+import de.htwg.se.VierGewinnt.model.playgroundComponent.playgroundBaseImpl.PlaygroundPvP
 import de.htwg.se.VierGewinnt.util.Move
 import play.api._
+import play.api.libs.json.{JsNumber, JsString, Json}
 import play.api.mvc._
 
 import javax.inject._
@@ -32,12 +36,12 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   def newGame(gameType: Int) = Action { implicit request: Request[AnyContent] =>
     controller.setupGame(gameType, 7)
-    Ok(views.html.index(controller.playground.grid, controller.printState, controller.playground.player.head.getName()))
+    Ok(pgToJson(controller.playground, controller.printState))
   }
 
   def insert(x: Int) = Action { implicit request: Request[AnyContent] =>
     controller.doAndPublish(controller.insChip, Move(x))
-    Ok(views.html.index(controller.playground.grid, controller.printState, controller.playground.player.head.getName()))
+    Ok(pgToJson(controller.playground, controller.printState))
   }
 
   def notFound() = Action { implicit request: Request[AnyContent] =>
@@ -50,5 +54,33 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   def gameIntro() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.gameIntro())
+  }
+
+  private def pgToJson(pg: PlaygroundInterface, state: String) = {
+    Json.obj(
+      "state" -> JsString(state),
+      "playground" -> Json.obj(
+        "size" -> JsNumber(pg.size),
+        "gameType" -> JsNumber(if (pg.isInstanceOf[PlaygroundPvP]) 0 else 1),
+        "currentPlayer" -> Json.obj(
+          "name" -> JsString(pg.player(0).getName()),
+          "chipColor" -> JsString(pg.player(0).getChip().toString)),
+        "otherPlayer" -> Json.obj(
+          "name" -> JsString(pg.player(1).getName()),
+          "chipColor" -> JsString(pg.player(1).getChip().toString)),
+        "cells" -> Json.toJson(
+          for {
+            row <- 0 until pg.size
+            col <- 0 until pg.size
+          } yield {
+            Json.obj(
+              "row" -> row,
+              "col" -> col,
+              "chip" -> pg.grid.getCell(row, col).chip.toString
+            )
+          }
+        )
+      )
+    )
   }
 }
--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -1,39 +1,76 @@
-const oGrid = document.getElementById("grid");
-const iGridSize = Number(document.getElementById("gridsize").innerHTML)
+function createBlankPlayground() {
+    this.playgroundExists = true;
+    const oGrid = document.getElementById("grid");
+    for (let iRow = 0; iRow < this.iGridSize; iRow++) {
+        const oRow = document.createElement("div")
+        oRow.className = "row d-flex justify-content-evenly"
 
+        for (let iCol = 0; iCol < this.iGridSize; iCol++) {
+            const oCellDiv = document.createElement("div");
+            oCellDiv.className = "col-1";
+            const oCellSpan = document.createElement("span");
+            oCellSpan.id = `${iRow}.${iCol}`
+            oCellSpan.className = "col bi bi-circle-fill";
+            oCellSpan.style = "font-size: 2em";
+            oCellSpan.classList.add("text");
 
-for (let iRow = 0; iRow < iGridSize; iRow++) {
-    const oRow = document.createElement("div")
-    oRow.className = "row d-flex justify-content-evenly"
+            oCellDiv.addEventListener('click', function () {
+                playMove(iCol)
+            });
+            oCellDiv.appendChild(oCellSpan)
+            oRow.appendChild(oCellDiv)
+        }
+        oGrid.appendChild(oRow)
+    }
+}
 
-    for (let iCol = 0; iCol < iGridSize; iCol++) {
-        const oCellDiv = document.createElement("div");
-        oCellDiv.className = "col-1";
-        const oCellSpan = document.createElement("span");
-        oCellSpan.className = "col bi bi-circle-fill";
-        oCellSpan.style = "font-size: 2em";
-
-
-        const sColor = document.getElementById(`${iRow}.${iCol}`).innerHTML
-
-        switch (sColor) {
+function update() {
+    this.aCells.forEach((cell) => {
+        let oCell = document.getElementById(`${cell.row}.${cell.col}`);
+        switch (cell.chip) {
             case "RED":
-                oCellSpan.classList.add("text-warning");
+                oCell.classList.add("text-warning");
                 break;
             case "YELLOW":
-                oCellSpan.classList.add("text-danger");
+                oCell.classList.add("text-danger");
                 break;
             default:
-                oCellSpan.classList.add("text");
+                oCell.classList.add("text");
         }
-        oCellDiv.addEventListener('click', function () {
-            playMove(iCol)
-        });
-        oCellDiv.appendChild(oCellSpan)
-        oRow.appendChild(oCellDiv)
+    })
 
+    for (let iRow = 0; iRow < this.iGridSize; iRow++) {
+        const oRow = document.createElement("div")
+        oRow.className = "row d-flex justify-content-evenly"
+
+        for (let iCol = 0; iCol < this.iGridSize; iCol++) {
+            const oCellDiv = document.createElement("div");
+            oCellDiv.className = "col-1";
+            const oCellSpan = document.createElement("span");
+            oCellSpan.className = "col bi bi-circle-fill";
+            oCellSpan.style = "font-size: 2em";
+
+
+            const sColor = document.getElementById(`${iRow}.${iCol}`).innerHTML
+
+            switch (sColor) {
+                case "RED":
+                    oCellSpan.classList.add("text-warning");
+                    break;
+                case "YELLOW":
+                    oCellSpan.classList.add("text-danger");
+                    break;
+                default:
+                    oCellSpan.classList.add("text");
+            }
+            oCellDiv.addEventListener('click', function () {
+                playMove(iCol)
+            });
+            oCellDiv.appendChild(oCellSpan)
+            oRow.appendChild(oCellDiv)
+
+        }
     }
-    oGrid.appendChild(oRow)
 }
 
 function playMove(column) {
@@ -44,5 +81,35 @@ function playMove(column) {
             'Content-Type': 'application/json'
         },
         body: "",
-    }).then(() => location.reload());
+    }).then((response) => {
+        response.json().then((data) => this._parsePlayground(data))
+    });
 }
+
+function newGame(type) {
+    fetch(`/newGame/${type}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: "",
+    }).then(() => {
+        this.iGridSize = 7;
+        if(!this.playgroundExists) {
+            this.createBlankPlayground()
+        }
+    });
+}
+
+function _parsePlayground(data){
+    const pg = data.playground;
+    this.sState = data.state;
+    this.iGridSize = pg.size;
+    this.iGametype = pg.gameType;
+    this.oCurrentPlayer = pg.currentPlayer;
+    this.oOtherPlayer = pg.otherPlayer;
+    this.aCells = pg.cells;
+    this.update();
+}
+
