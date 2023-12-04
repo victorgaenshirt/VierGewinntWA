e343942bc042789525072170378a5c80e8ea17e9
victorgaenshirt <vgaenshirt@posteo.de>
2023-12-04 16:03:37 +0100

    connect Websocket function

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -1,3 +1,30 @@
+function connectWebSocket() {
+    console.log("connectWebSocket");
+    var webSocket = new WebSocket("ws://localhost:9000/websocket");
+    console.log("connected");
+    webSocket.onopen = function (event) {
+        console.log("onopen");
+        webSocket.send("Trying to connect");
+    }
+    webSocket.onclose = function (event) {
+        console.log("onclose");
+    }
+    webSocket.onerror = function (error) {
+        console.log("Error" + error + "occurred");
+    }
+    webSocket.onmessage = function (event) {
+        if(typeof event.data === "string"){
+            console.log('String message received: ' + event.data);
+        }
+        else if(event.data instanceof ArrayBuffer){
+            console.log('ArrayBuffer received: ' + event.data);
+        }
+        else if(event.data instanceof Blob){
+            console.log('Blob received: ' + event.data);
+        }
+    }
+}
+
 function createBlankPlayground() {
     this.playgroundExists = true;
     const oGrid = document.getElementById("grid");
@@ -205,3 +232,5 @@ function suggestion(){
         oElement.innerHTML = `AI suggests Player ${data.player} to play column: ${data.suggestedColumn}`
     }));
 }
+
+
